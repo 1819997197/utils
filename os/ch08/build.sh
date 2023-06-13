bca7d3066e74f58844d8b07a78046537da179e53
will <573583868@qq.com>
2023-06-13 21:35:41 +0800

    os

--- os/ch08/build.sh
+++ os/ch08/build.sh
@@ -13,8 +13,10 @@ gcc -m32 -I lib/kernel/ -m32 -I lib/ -m32 -I kernel/ -c -fno-builtin -o build/ma
 gcc -m32 -I lib/kernel/ -m32 -I lib/ -m32 -I kernel/ -c -fno-builtin -o build/interrupt.o kernel/interrupt.c
 gcc -m32 -I lib/kernel/ -m32 -I lib/ -m32 -I kernel/ -c -fno-builtin -o build/init.o kernel/init.c
 gcc -m32 -I lib/kernel/ -m32 -I lib/ -m32 -I kernel/ -c -fno-builtin -o build/debug.o kernel/debug.c
-gcc -m32 -I lib/kernel/ -m32 -I lib/ -m32 -I kernel/ -c -fno-builtin -o build/string.o kernel/string.c
+gcc -m32 -I lib/kernel/ -m32 -I lib/ -m32 -I kernel/ -c -fno-builtin -o build/string.o lib/string.c
+gcc -m32 -I lib/kernel/ -m32 -I lib/ -m32 -I kernel/ -c -fno-builtin -o build/bitmap.o lib/kernel/bitmap.c
+gcc -m32 -I lib/kernel/ -m32 -I lib/ -m32 -I kernel/ -c -fno-builtin -o build/memory.o kernel/memory.c
 nasm -f elf -o build/print.o lib/kernel/print.S
 nasm -f elf -o build/kernel.o kernel/kernel.S
-ld -m elf_i386 -Ttext 0xc0001500 -e main -o build/kernel.bin build/main.o build/init.o build/interrupt.o  build/print.o build/kernel.o build/timer.o build/debug.o build/string.o
+ld -m elf_i386 -Ttext 0xc0001500 -e main -o build/kernel.bin build/main.o build/init.o build/interrupt.o  build/print.o build/kernel.o build/timer.o build/debug.o build/string.o build/memory.o build/bitmap.o
 dd if=/data/os/ch08/build/kernel.bin of=/data/os/c.img bs=512 count=200 seek=9 conv=notrunc
